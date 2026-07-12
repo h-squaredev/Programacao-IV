9457b9d58d3248d84f9d5e01fb02abbc6e277b3a
Humberto Henrique <humbertohenrique04@gmail.com>
2026-07-12 19:01:56 -0300

    feat(user controller): mapa de requisicoes HTTP para PessoaService

--- Task06Aula07/backend/src/users/users.controller.ts
+++ Task06Aula07/backend/src/users/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { PessoaService } from './pessoa.service';
+import { CreateUserDto } from './create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly pessoaService: PessoaService) {}
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto) {
+    return this.pessoaService.create(createUserDto);
+    }
+
+    @Get()
+    findAll() {
+    return this.pessoaService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+    return this.pessoaService.findOne(id);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+    return this.pessoaService.remove(id);
+    }
+}
