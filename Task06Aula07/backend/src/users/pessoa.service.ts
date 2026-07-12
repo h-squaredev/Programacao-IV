05c46130c4c123b59aa36cf342926afdd6ba727d
Humberto Henrique <humbertohenrique04@gmail.com>
2026-07-12 18:34:30 -0300

    feat(typeORM repository): injetar o repositorio e criar funcoes para o banco de dados

--- Task06Aula07/backend/src/users/pessoa.service.ts
+++ Task06Aula07/backend/src/users/pessoa.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './create-user.dto';
 
 @Injectable()
-export class PessoaService {}
+export class PessoaService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ){}
+
+    create(createUserDto: CreateUserDto) {
+        return this.userRepository.save(createUserDto);
+    }
+
+    findAll() {
+        return this.userRepository.find();
+    }
+
+    findOne(id: number) {
+        return this.userRepository.findOneBy({ id: id });
+    }
+
+    remove(id: number) {
+        return this.userRepository.delete(id);
+    }
+}
