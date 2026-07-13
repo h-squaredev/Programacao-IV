664a8ab5b2eb975dbb2606c12120b5b08d9705b4
Humberto Henrique <humbertohenrique04@gmail.com>
2026-07-13 10:12:50 -0300

    chore(database): mudanca de database e sqlite para mysql

--- TasksBackEnd-07a12/src/ormconfig.ts
+++ TasksBackEnd-07a12/src/ormconfig.ts
@@ -1,8 +1,21 @@
-import { DataSourceOptions } from 'typeorm';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
-export const config: DataSourceOptions = {
-    type: 'sqlite',
-    database: 'db.sqlite',
-    synchronize: true,
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-};
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306, // porta padrão do MySQL 
+      username: 'root', // usuário do servidor MySQL
+      password: 'TInaveia123@', // senha do servidor MySQL
+      database: 'atividades_prog', // O banco de dados isolado
+      entities: [User],
+      synchronize: true, // Permite que o TypeORM crie a tabela automaticamente no MySQL
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
