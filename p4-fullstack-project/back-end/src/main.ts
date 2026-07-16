2201b888a678d4bd5942ddd93cad58b10dbb567e
Humberto Henrique <humbertohenrique04@gmail.com>
2026-07-16 09:15:44 -0300

    feat: Implementação de Swagger

--- p4-fullstack-project/back-end/src/main.ts
+++ p4-fullstack-project/back-end/src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // <-- Importado aqui
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Habilita o CORS: Permite que o Front-end se conecte à API sem ser bloqueado pelo navegador
-  app.enableCors(); 
+  
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
-  app.useGlobalPipes(new ValidationPipe());   // Ativa a validacao automatica em todas as rotas da API
-  const port = process.env.PORT || 3000; // Usa a porta definida na variável de ambiente ou 3000 como padrão
+  // CONFIGURAÇÃO DO SWAGGER (Adicione este bloco antes do app.listen)
+  const config = new DocumentBuilder()
+    .setTitle('API de Conteúdo Dinâmico - Trabalho P4')
+    .setDescription('Documentação interativa para a API de Posts e Autenticação JWT')
+    .setVersion('1.0')
+    .addBearerAuth() // Permite colar o Token JWT para testar as rotas protegidas
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Define que a documentação ficará na rota /api
+
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Aplicação rodando na porta ${port}`);
 }
