2d5dc8dfd77e0cb273725b7001c13fa2dde99915
Humberto Henrique <humbertohenrique04@gmail.com>
2026-07-16 00:13:28 -0300

    Refactor: Solucionando problema com dist e inicialização do JWT

--- p4-fullstack-project/back-end/src/auth/auth.module.ts
+++ p4-fullstack-project/back-end/src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // <-- Importados para carregar as chaves de forma segura
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
@@ -10,9 +11,14 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET, // Chave secreta para assinar o token JWT
-      signOptions: { expiresIn: '1h' },        // Token expira em 1 hora
+    // Substituído register por registerAsync para evitar a brecha de inicialização
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') || 'MINHA_CHAVE_SUPER_SECRETA_P4',
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
   ],
   controllers: [AuthController],
