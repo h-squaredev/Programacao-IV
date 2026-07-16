3bf7e190a7e203b9072c338e74b35729d0534c5a
Humberto Henrique <humbertohenrique04@gmail.com>
2026-07-16 01:01:00 -0300

    feat(src): liberando CORS no Back-end

--- p4-fullstack-project/back-end/src/main.ts
+++ p4-fullstack-project/back-end/src/main.ts
@@ -4,6 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilita o CORS: Permite que o Front-end se conecte à API sem ser bloqueado pelo navegador
+  app.enableCors(); 
+
   app.useGlobalPipes(new ValidationPipe());   // Ativa a validacao automatica em todas as rotas da API
   const port = process.env.PORT || 3000; // Usa a porta definida na variável de ambiente ou 3000 como padrão
   await app.listen(port);
